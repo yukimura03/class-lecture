4f4010bf934299ac442023cae94464d1cd3319f7
yukimura03 <4leaf.caffe.latte@gmail.com>
2018-12-09 01:15:30 +0900

    Lecture5 finish

---
+++ ClassLecture5/ClassLecture5/CopyMachine.swift
@@ -0,0 +1,51 @@
+//
+//  CopyMachine.swift
+//  ClassLecture5
+//
+//  Created by minagi on 2018/12/09.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+class CopyMachine {
+    var paper : Int
+    //コピー可能な枚数
+    
+    init(sheet: Int){
+        //最初に給紙する枚数を指定する
+        //枚数がマイナスの場合は0枚に設定する
+        if sheet < 0 {
+            paper = 0
+        } else {
+            paper = sheet
+        }
+    }
+    
+    
+    func feedPaper(sheet: Int){
+        if sheet >= 0 {
+            print("\(sheet)枚給紙しました。")
+            paper += sheet
+        }
+    }
+    //引数として渡された枚数だけ給紙する
+    
+    func copy(sheet: Int) {
+        if sheet > paper {
+            print("\(paper)枚コピーしました")
+            paper = 0
+        } else {
+            print("\(sheet)枚コピーしました")
+            paper -= sheet
+        }
+    }
+    //引数として渡された枚数をコピーする
+    //すべてをコピーできない場合は可能な枚数だけコピーする
+    
+    func display(){
+        print("コピー可能な枚数は\(paper)枚です。")
+    }
+    //コピー可能な枚数を表示する
+    
+}
---
+++ ClassLecture5/ClassLecture5/main.swift
@@ -0,0 +1,45 @@
+//
+//  main.swift
+//  ClassLecture5
+//
+//  Created by minagi on 2018/12/09.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+func main(){
+    
+    print("最初に給紙する枚数を入力してください >")
+    let copyMachine = CopyMachine(sheet: KeyboardUtils.inputInt())
+    
+    while true {
+        
+        print("\n1. 給紙 / 2. コピー / 3. 表示 / 4. 終了 >")
+        let no = KeyboardUtils.inputInt()
+        
+        switch no {
+        case 1:
+            print("給紙する枚数を入力してください >")
+            copyMachine.feedPaper(sheet: KeyboardUtils.inputInt())
+            break
+            
+        case 2:
+            print("コピーする枚数を入力してください >")
+            copyMachine.copy(sheet: KeyboardUtils.inputInt())
+            break
+            
+        case 3:
+            copyMachine.display()
+            break
+            
+        default:
+            print("アプリケーションを終了します。")
+            return
+        }
+        
+    }
+    
+}
+
+main()
