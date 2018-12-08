f603f5634737f48d54aa8623b7a3ada34f5f98bb
yukimura03 <4leaf.caffe.latte@gmail.com>
2018-12-09 01:49:58 +0900

    Lecture6 finish

---
+++ ClassLecture6/ClassLecture6/Pool.swift
@@ -0,0 +1,49 @@
+//
+//  Pool.swift
+//  ClassLecture6
+//
+//  Created by minagi on 2018/12/09.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+class Pool {
+    var water : Double
+    
+    init(water: Double){
+        self.water = water
+    }
+    
+    
+    func feedWater(water: Double){
+        if self.water + water <= 600 {
+            print("\(water)立方メートル給水しました。")
+            self.water += water
+        } else {
+            print("\(600 - self.water)立方メートル給水しました。")
+            self.water = 600
+        }
+        
+    }
+    //引数として渡された水量だけ給水する
+    //ただし満水（600.0立方メートル）を超える分は給水しない
+    
+    func drainWater(water: Double){
+        if self.water - water >= 0 {
+            print("\(water)立方メートル排水しました。")
+            self.water -= water
+        } else {
+            print("\(self.water)立方メートル排水しました。")
+            self.water = 0
+        }
+    }
+    //引数として渡された水量だけ排水する
+    //ただし空（0.0立方メートル）になるとそれ以上排水しない
+    
+    func display(){
+        print("現在の水量は\(water)立方メートルです。")
+    }
+    //現在の水量を表示する
+    
+}
---
+++ ClassLecture6/ClassLecture6/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  ClassLecture6
+//
+//  Created by minagi on 2018/12/09.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+func main(){
+    
+    let pool = Pool(water: 0)
+    
+    while true {
+        
+        print("\n1. 給水 / 2. 排水 / 3. 表示 / 4. 終了 >")
+        let no = KeyboardUtils.inputInt()
+        
+        switch no {
+        case 1:
+            print("給水する水量を入力して下さい >")
+            pool.feedWater(water: KeyboardUtils.inputDouble())
+            break
+            
+        case 2:
+            print("排水する水量を入力してください >")
+            pool.drainWater(water: KeyboardUtils.inputDouble())
+            break
+            
+        case 3:
+            pool.display()
+            break
+            
+        default :
+            print("アプリケーションを終了します")
+            return
+        }
+        
+    }
+    
+}
+
+main()
