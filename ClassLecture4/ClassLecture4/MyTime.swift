51f20ea71519b019fcb9392c4a6478a6c7befed6
yukimura03 <4leaf.caffe.latte@gmail.com>
2018-12-09 00:05:43 +0900

    lesson4 finish

---
+++ ClassLecture4/ClassLecture4/MyTime.swift
@@ -0,0 +1,87 @@
+//
+//  MyTime.swift
+//  ClassLecture3
+//
+//  Created by minagi on 2018/12/08.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+class MyTime {
+    
+    var hour : Int
+    var minute : Int
+    var second : Int
+    
+    
+    init(hour:Int, minute: Int, second: Int){
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+     }
+    
+    func setTime(hour:Int){
+        if hour < 24 {
+            self.hour = hour
+        } else {
+            self.hour = 0
+        }
+    }
+    
+    func setTime(minute:Int){
+        if minute < 60 {
+            self.minute = minute
+        } else {
+            self.minute = 0
+        }
+    }
+    
+    func setTime(second:Int){
+        if second < 60 {
+            self.second = second
+        } else {
+            self.second = 0
+        }
+    }
+    
+    
+    func getTime(){
+        print("\(NSString(format: "%02d",hour)):\(NSString(format: "%02d",minute)):\(NSString(format: "%02d",second))に設定されました")
+    }
+    
+    func increaseSecond(forward: Int) {
+        second += forward
+        while second >= 60 {
+            second -= 60
+            minute += 1
+            if minute >= 60 {
+                minute -= 60
+                hour += 1
+                if hour >= 24 {
+                    hour -= 24
+                }
+            }
+        }
+       print("\(NSString(format: "%02d",hour)):\(NSString(format: "%02d",minute)):\(NSString(format: "%02d",second))になりました")
+    }
+    //…　時刻をn秒進める
+    
+    func decreaseSecond(back: Int) {
+        second -= back
+        while second < 0 {
+            minute -= 1
+            second += 60
+            if minute < 0 {
+                hour -= 1
+                minute += 60
+                if hour < 0 {
+                    hour += 24
+                }
+            }
+        }
+        print("\(NSString(format: "%02d",hour)):\(NSString(format: "%02d",minute)):\(NSString(format: "%02d",second))になりました")
+    }
+    //…　時刻をn秒戻す
+    
+}
---
+++ ClassLecture4/ClassLecture4/main.swift
@@ -0,0 +1,50 @@
+//
+//  main.swift
+//  ClassLecture4
+//
+//  Created by minagi on 2018/12/08.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+func main() {
+    
+    let myTime = MyTime(hour: 0, minute: 0, second: 0)
+    
+    
+        print("時を入力　＞")
+        myTime.setTime(hour: KeyboardUtils.inputInt())
+        
+        print("分を入力　＞")
+        myTime.setTime(minute: KeyboardUtils.inputInt())
+        
+        print("秒を入力　＞")
+        myTime.setTime(second: KeyboardUtils.inputInt())
+        
+        myTime.getTime()
+ 
+    while true {
+        
+        print("\n1. 進める / 2. 戻す / 3. 終了 >")
+        let no = KeyboardUtils.inputInt()
+    
+        switch no {
+        case 1:
+            print("何秒進めるか ＞")
+            myTime.increaseSecond(forward: KeyboardUtils.inputInt())
+            break
+            
+        case 2:
+            print("何秒戻すか ＞")
+            myTime.decreaseSecond(back: KeyboardUtils.inputInt())
+            break
+            
+        default:
+            print("アプリケーションを終了します。")
+            return
+        }
+    }
+}
+
+main()
