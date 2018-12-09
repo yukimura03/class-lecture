89a8e891e1b832f11c9aeff759f2a85ac472e027
yukimura03 <4leaf.caffe.latte@gmail.com>
2018-12-09 23:23:29 +0900

    Lecture8 finish

---
+++ ClassLecture8/ClassLecture8/Box.swift
@@ -0,0 +1,53 @@
+//
+//  Box.swift
+//  ClassLecture8
+//
+//  Created by minagi on 2018/12/09.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+class Box {
+    
+    var color : String //箱の色
+    var shape : String //箱の形
+    var num : Int //箱の中のボールの数
+    static var totalNumber = 0 //全ての箱のボールの総数
+    
+    init(color: String, shape: String, num: Int) {
+        self.color = color
+        self.shape = shape
+        self.num = num
+    }
+    
+    
+    func insertBalls(balls: Int){
+        print("\(balls)個のボールを入れました。")
+        num += balls
+    }
+    //引数として渡された個数のボールを箱に入れる
+    
+    func removeBalls(balls: Int){
+        if balls >= num {
+            print("\(balls)個のボールを取り出しました。")
+            num -= balls
+        } else {
+            print("\(num)個のボールを取り出しました。")
+            num = 0
+        }
+    }
+    //引数として渡された個数のボールを箱から取り出す
+    //個数が不足する場合はすべてのボールを取り出す
+    
+    func displayInfo(boxName: Int){
+        print("\(boxName)---色：\(color), 形：\(shape), 個数：\(num)")
+    }
+    //箱の色・形、ボールの数を表示する
+    
+    static func displayTotalNumber(totalNumber: Int){
+        print("ボールの合計は\(totalNumber)個です。")
+    }
+    //すべての箱のボールの総数を表示する
+    
+}
---
+++ ClassLecture8/ClassLecture8/main.swift
@@ -0,0 +1,78 @@
+//
+//  main.swift
+//  ClassLecture8
+//
+//  Created by minagi on 2018/12/09.
+//  Copyright © 2018 minagi. All rights reserved.
+//
+
+import Foundation
+
+func main(){
+    
+    let box0 = Box(color: "赤", shape: "円形", num: 1)
+    let box1 = Box(color: "青", shape: "四角", num: 2)
+    
+    
+    while true {
+        
+        print("\n1. ボール投入 / 2. ボール取出 / 3. ボール総数表示 / 4. 終了 >")
+        let no = KeyboardUtils.inputInt()
+        
+        switch no {
+            
+        case 1:
+            box0.displayInfo(boxName: 0)
+            box1.displayInfo(boxName: 1)
+            
+            print("ボールを投入する箱の番号を入力して下さい >")
+            let boxName = KeyboardUtils.inputInt()
+            
+            switch boxName {
+                case 0:
+                    print("投入するボールの個数を入力して下さい >")
+                    box0.insertBalls(balls: KeyboardUtils.inputInt())
+                    break
+                case 1:
+                    print("投入するボールの個数を入力して下さい >")
+                    box1.insertBalls(balls: KeyboardUtils.inputInt())
+                    break
+                default:
+                    print("箱の番号の入力が誤っています。")
+                }
+            break
+            
+        case 2:
+            box0.displayInfo(boxName: 0)
+            box1.displayInfo(boxName: 1)
+            
+            print("ボールを取り出す箱の番号を入力して下さい >")
+            let boxName = KeyboardUtils.inputInt()
+            
+            switch boxName {
+                case 0:
+                    print("取り出すボールの個数を入力して下さい >")
+                    box0.removeBalls(balls: KeyboardUtils.inputInt())
+                    break
+                case 1:
+                    print("取り出すボールの個数を入力して下さい >")
+                    box1.removeBalls(balls: KeyboardUtils.inputInt())
+                    break
+                default:
+                    print("箱の番号の入力が誤っています。")
+                }
+            break
+            
+        case 3:
+            let total = box0.num + box1.num
+            Box.displayTotalNumber(totalNumber: total)
+            break
+            
+        default :
+            print("アプリケーションを終了します")
+            return
+        }
+    }
+}
+
+main()
